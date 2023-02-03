c354e3fb634b81e93dc56ac7ec870fef9bf2261d
Fahadmohammad1 <205fahadmohammad@gmail.com>
2023-02-03 23:51:26 +0600

    pass revealed again

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-const uri = `mongodb+srv://marsUser:${process.env.DB_PASS}@cluster0.ejds7.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://marsUser:IygYwT2DukLrDewy@cluster0.ejds7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
