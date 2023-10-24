4396e575766fc7493df8fb9e5128f4c287a159e7
Fahad <205fahadmohammad@gmail.com>
2023-10-24 16:43:28 +0600

    origin added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://mars-technology-630b3.web.app" }));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ejds7.mongodb.net/?retryWrites=true&w=majority`;
